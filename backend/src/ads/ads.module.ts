796feea64045223edc256fcadebc4d342f5a7c0c
abi677715-del <abi677715@gmail.com>
2026-07-28 18:36:11 +0300

    Update ads.module.ts

--- backend/src/ads/ads.module.ts
+++ backend/src/ads/ads.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdsService } from './ads.service';
 import { AdsController } from './ads.controller';
+import { AdAutoPauseService } from './ad-auto-pause.service';
 import { KycModule } from '../kyc/kyc.module';
 
 @Module({
   imports: [KycModule],
   controllers: [AdsController],
-  providers: [AdsService],
+  providers: [AdsService, AdAutoPauseService],
   exports: [AdsService],
 })
 export class AdsModule {}
