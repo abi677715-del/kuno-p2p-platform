e0c92fea9b71544e62823c498d68009b93a6932b
abi677715-del <abi677715@gmail.com>
2026-07-29 15:51:25 +0300

    Update wallet.module.ts

--- backend/src/wallet/wallet.module.ts
+++ backend/src/wallet/wallet.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { DepositWatcherService } from './deposit-watcher.service';
 
 @Module({
   controllers: [WalletController],
-  providers: [WalletService],
+  providers: [WalletService, DepositWatcherService],
   exports: [WalletService],
 })
 export class WalletModule {}
