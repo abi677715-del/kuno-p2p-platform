22be1514910cb6e1cf25b2ec83f3eedeca3a4a03
abi677715-del <abi677715@gmail.com>
2026-07-30 16:35:07 +0300

    Update ads.module.ts

--- backend/src/ads/ads.module.ts
+++ backend/src/ads/ads.module.ts
@@ -3,9 +3,10 @@ import { AdsService } from './ads.service';
 import { AdsController } from './ads.controller';
 import { AdAutoPauseService } from './ad-auto-pause.service';
 import { KycModule } from '../kyc/kyc.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [KycModule],
+  imports: [KycModule, UsersModule],
   controllers: [AdsController],
   providers: [AdsService, AdAutoPauseService],
   exports: [AdsService],
