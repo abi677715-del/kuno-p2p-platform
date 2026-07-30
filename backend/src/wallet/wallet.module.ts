0cd8d9b16e62b6af8ff38ca4fe84730898660c3f
abi677715-del <abi677715@gmail.com>
2026-07-30 13:04:29 +0300

    Update wallet.module.ts

--- backend/src/wallet/wallet.module.ts
+++ backend/src/wallet/wallet.module.ts
@@ -3,8 +3,10 @@ import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
 import { DepositWatcherService } from './deposit-watcher.service';
 import { WithdrawalSenderService } from './withdrawal-sender.service';
+import { KycModule } from '../kyc/kyc.module';
 
 @Module({
+  imports: [KycModule],
   controllers: [WalletController],
   providers: [WalletService, DepositWatcherService, WithdrawalSenderService],
   exports: [WalletService],
