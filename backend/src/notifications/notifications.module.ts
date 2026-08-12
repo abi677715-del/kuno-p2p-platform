afd5053025c0a07758b34371f7d4654a776ad5b4
abi677715-del <abi677715@gmail.com>
2026-08-12 12:24:43 +0300

    Add PushService to NotificationsModule providers

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsGateway } from './notifications.gateway';
+import { PushService } from './push.service';
 import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [MailModule],
   controllers: [NotificationsController],
-  providers: [NotificationsService, NotificationsGateway],
+  providers: [NotificationsService, NotificationsGateway, PushService],
   exports: [NotificationsService],
 })
 export class NotificationsModule {}
