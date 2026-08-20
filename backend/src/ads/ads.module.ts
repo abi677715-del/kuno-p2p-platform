622552386e9bdc337f81d68d3b44b6345e0ce3ed
abi677715-del <abi677715@gmail.com>
2026-08-20 19:12:57 +0300

    Add RelationsModule to AdsModule imports

--- backend/src/ads/ads.module.ts
+++ backend/src/ads/ads.module.ts
@@ -4,9 +4,10 @@ import { AdsController } from './ads.controller';
 import { AdAutoPauseService } from './ad-auto-pause.service';
 import { KycModule } from '../kyc/kyc.module';
 import { UsersModule } from '../users/users.module';
+import { RelationsModule } from '../relations/relations.module';
 
 @Module({
-  imports: [KycModule, UsersModule],
+  imports: [KycModule, UsersModule, RelationsModule],
   controllers: [AdsController],
   providers: [AdsService, AdAutoPauseService],
   exports: [AdsService],
